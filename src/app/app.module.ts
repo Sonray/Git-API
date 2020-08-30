5f2eeac778d66c79bb19185c21ea6beeb5c50153
David Okwacha <osweredavid@gmail.com>
2020-08-30 13:08:32 +0300

    generarted date pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
