037d07e18d0c06a99e6f257194b5cd72bea7974d
David Okwacha <osweredavid@gmail.com>
2020-08-30 14:04:33 +0300

    Added Profile htnl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DatePipe } from './date.pipe';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { DatePipe } from './date.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
