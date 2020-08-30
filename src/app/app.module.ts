cac01867902200ee7a86d0ae48b7ede0b5d3351b
David Okwacha <osweredavid@gmail.com>
2020-08-30 14:07:43 +0300

    generated not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DatePipe } from './date.pipe';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    DatePipe
+    DatePipe,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
