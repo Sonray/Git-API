c2b2ac67e48b9da93001a0c472d544f0436aa58a
David Okwacha <osweredavid@gmail.com>
2020-08-30 11:45:51 +0300

    Declared the user profile Oath keys

--- src/app/user-profile.service.ts
+++ src/app/user-profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
 
-  constructor() { }
+  private username:string;
+  private client_id = "4b1a2fadefdfd945db5d";
+  private client_secret ="d1c3716ce71eeab26a8f8b5aa9f090638b129cce";
+  
+  constructor() { 
+    
+  }
 }
